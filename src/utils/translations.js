26f8281c3ddb99e47e3d2fc682d1cf2fb08f9276
evaaaaawu <yiju.wu1@gmail.com>
2026-03-18 15:01:41 +0800

    docs: delete about me

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -7,13 +7,11 @@ const translations = {
       info2: 'If you want to choose articles by topic, click the "Tags" page.',
       info3:
         'If you want to browse all of my articles, click the "Articles" page.',
-      info4: 'If you want to learn more about me, click the "About" page.',
     },
     nav: {
       home: "Home",
       tags: "Tags",
       articles: "Articles",
-      about: "About",
     },
     common: {
       readMore: "Read Full Article",
@@ -36,13 +34,11 @@ const translations = {
       info1: "繼續往下滑可以看到我推薦的精選文章。",
       info2: "如果想要依主題選擇文章，可以點擊「主題文章」頁面。",
       info3: "如果想要瀏覽我的所有文章，可以點擊「所有文章」頁面。",
-      info4: "如果想要了解關於我的資訊，歡迎點擊「關於我」頁面。",
     },
     nav: {
       home: "首頁",
       tags: "主題文章",
       articles: "所有文章",
-      about: "關於我",
     },
     common: {
       readMore: "閱讀全文",
