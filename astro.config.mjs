5189b26bc51568524b95e3227c16b8113952ad20
evaaaaawu <yiju.wu1@gmail.com>
2026-04-16 22:37:34 +0800

    feat(i18n): configure astro i18n with en default and zh-tw locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ export default defineConfig({
   site: "https://evaaaaawu.com",
   output: "static",
   integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "zh-tw"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
